aae3116a46f7fd2f91707a17f2eff3b97bc74a6f
Steve JobsEleanor <eleanorabrow@gmail.com>
2023-04-29 11:22:56 -0400

    pulled in api and displayed data

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import Home from './components/Home/index.js'
-import Definition from './components/Definition/index.js'
-
+import React, { useState } from 'react';
 
 function App() {
+  const [word, setWord] = useState('');
+  const [definition, setDefinition] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then(response => response.json())
+      .then(data => {
+        const definition = data[0].meanings[0].definitions[0].definition;
+        setDefinition(definition);
+      })
+      .catch(error => console.error(error));
+  }
+
   return (
-    <div className="App">
-      <Home />
-      <Definition />
+    <div>
+      <h1>Dictionary</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Word:
+          <input type="text" value={word} onChange={event => setWord(event.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {definition && <p>{definition}</p>}
     </div>
   );
 }
